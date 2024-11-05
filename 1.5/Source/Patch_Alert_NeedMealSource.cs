e1a77748803e5ea834436c63f1d6fca63e2051f7
1trickPwnyta <sarahpace37@gmail.com>
2024-11-05 13:28:12 -0800

    Disable need meal source alert when using kibble precept

---
+++ 1.5/Source/Patch_Alert_NeedMealSource.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Linq;
+using Verse;
+
+namespace KibblePrecept
+{
+    [HarmonyPatch(typeof(Alert_NeedMealSource))]
+    [HarmonyPatch("NeedMealSource")]
+    public static class Patch_Alert_NeedMealSource
+    {
+        public static void Postfix(Map map, ref bool __result)
+        {
+            if (__result)
+            {
+                if (map.mapPawns.FreeColonistsAndPrisonersSpawned.Where(p => !p.DevelopmentalStage.Baby()).All(p => p.Ideo != null && p.Ideo.HasPrecept(DefDatabase<PreceptDef>.GetNamed("KibbleEating_DontMind"))))
+                {
+                    if (map.listerBuildings.allBuildingsColonist.Any(b => b.def.recipes != null && b.def.recipes.Contains(DefDatabase<RecipeDef>.GetNamed("Make_Kibble"))))
+                    {
+                        __result = false;
+                    }
+                }
+            }
+        }
+    }
+}
