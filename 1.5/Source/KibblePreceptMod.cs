a1d70ecef0ca5b3076ec16b8d07c104a3e908018
1trickPwnyta <sarahpace37@gmail.com>
2024-11-13 15:03:55 -0800

    Fix food optimality and preferability for kibble eaters

--- 1.5/Source/KibblePreceptMod.cs
+++ 1.5/Source/KibblePreceptMod.cs
@@ -1,4 +1,6 @@
 using HarmonyLib;
+using RimWorld;
+using System.Reflection;
 using Verse;
 
 namespace KibblePrecept
@@ -12,6 +14,7 @@ namespace KibblePrecept
         {
             var harmony = new Harmony(PACKAGE_ID);
             harmony.PatchAll();
+            harmony.Patch(typeof(FoodUtility).GetNestedType("<>c__DisplayClass14_0", BindingFlags.NonPublic).Method("<BestFoodSourceOnMap>b__0"), null, null, typeof(Patch_FoodUtility_BestFoodSourceOnMap).Method(nameof(Patch_FoodUtility_BestFoodSourceOnMap.Transpiler)));
 
             Log.Message($"[{PACKAGE_NAME}] Loaded.");
         }
